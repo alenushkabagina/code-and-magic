04da58324141ec36bd9a62bb90a80da5ce98f6a3
AizeRs <NAKorshunov@mail.ru>
2021-02-16 18:46:24 +0500

    Задание 3

---
+++ js/setup.js
@@ -0,0 +1,5 @@
+let Character = [
+Name = [Иван, Хуан Себастьян, Мария, Кристоф, Виктор, Юлия, Люпита, Вашингтон],
+CoatColor = [rgb(101, 137, 164), rgb(241, 43, 107), rgb(146, 100, 161), rgb(56, 159, 117), rgb(215, 210, 55), rgb(0, 0, 0)],
+EyesColor = [black, red, blue, yellow, green],
+];
