5a68009c4fd48c6ba03e69b522405544d0472080
AizeRs <NAKorshunov@mail.ru>
2021-03-02 19:52:01 +0500

    Задание 3

--- js/setup.js
+++ js/setup.js
@@ -1,13 +1,19 @@
 // const Иван = 'строка имени';
+
+const surnames = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
 const names = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
-const coatColor = [rgb(101, 137, 164), rgb(241, 43, 107), rgb(146, 100, 161), rgb(56, 159, 117), rgb(215, 210, 55), rgb(0, 0, 0)],
-const eyesColor = [black, red, blue, yellow, green],
+const coatColores = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
+const eyesColores = ['black', 'red', 'blue', 'yellow', 'green'];
 
 const generateWizard = () => {
+
+    surname: surnames[getRandomInt(0, surnames.length)],
+    // coatColor: coatColores[getRandomInt(0, coatColores.length)],
+    // eyesColor: eyesColores[getRandomInt(0, eyesColores.length)],
   return {
+
     name: names[getRandomInt(0, names.length)],
-    coatColor: coatColor[getRandomInt(0, coatColor.length)],
-    eyesColor: eyesColor[getRandomInt(0, eyesColor.length)],
+
   };
 }
 
@@ -19,4 +25,4 @@ const generateWizards = (number) => {
   return arr;
 }
 
-console.log(generateWizards(4))
+// console.log(generateWizards(4))
